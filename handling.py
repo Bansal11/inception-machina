bf30712f19964b6f478ac7495730519b325aebec
bansal11 <bhavya.bansal1998@gmail.com>
2018-07-04 10:47:39 +0530

    Error Handing

---
+++ handling.py
@@ -0,0 +1,14 @@
+
+try:
+    f = open("abc.txt")
+    b = 1/0
+#except Exception as e:
+    print(e)
+#except ZeroDivisionError:
+    print("divided by zero")
+#except FileNotFoundError:
+    print("file not find")
+except (FileNotFoundError, ZeroDivisionError):
+    print("sdsdsddsdsdsd")
+
+#i = 0/0
